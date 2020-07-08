8c9c93e8acb951a79c0ba2dd636964b772eaef3f
omarieclaire <marieflanagan@gmail.com>
2020-07-07 21:35:56 -0400

    working?

--- js/sketch.js
+++ js/sketch.js
@@ -1,24 +1,12 @@
-var database;
-var drawing = [];
-var hardtileId = 1;
-var currentTile = hardtileId;
-var tiles = {
+let database;
+let drawing = [];
+let tileId = 1;
+let currentTile = tileId;
+let tiles = {
   1: {
     'writing': 'writing',
-    'drawing': drawing,
+    'drawing': [],
     'tile': 1,
-    'firebaseKey' : null,
-    'width': 70,
-    'height': 40,
-    'position': {
-      'x': 10,
-      'y': 10
-    }
-  },
-  2: {
-    'writing': 'writing',
-    'drawing': drawing,
-    'tile': 2,
     'firebaseKey' : null,
     'width': 70,
     'height': 40,
@@ -26,14 +14,26 @@ var tiles = {
       'x': 10,
       'y': 50
     }
+  },
+  2: {
+    'writing': 'writing',
+    'drawing': [],
+    'tile': 2,
+    'firebaseKey' : null,
+    'width': 70,
+    'height': 40,
+    'position': {
+      'x': 10,
+      'y': 100
+    }
   }
 };
-var writing;
-var currentPath = []; // (ARRAY WHERE THE CURRENT DRAWING IS BEING STORED)
-var isDrawing = false;
-var drawCanvasToggle = false;
-// var tileFirebaseMap = {}; // empty javascript object / map
-var bg;
+let writing;
+let currentPath = []; // (ARRAY WHERE THE CURRENT DRAWING IS BEING STORED)
+let isDrawing = false;
+let drawCanvasToggle = false;
+// let tileFirebaseMap = {}; // empty javascript object / map
+let bg;
 
 //SETUP
 function setup() {
@@ -60,11 +60,11 @@ function setup() {
 
   // FIREBASE AUTH STUFF
   var config = {
-    apiKey: "AIzaSyAkqkz-UZyRSv_1QgfMjUeqX8mjZfg0MJE",
-    authDomain: "throne-room-club.firebaseapp.com",
-    databaseURL: "https://throne-room-club.firebaseio.com",
-    storageBucket: "throne-room-club.appspot.com",
-    messagingSenderId: "889776405480"
+    apiKey: 'AIzaSyAkqkz-UZyRSv_1QgfMjUeqX8mjZfg0MJE',
+    authDomain: 'throne-room-club.firebaseapp.com',
+    databaseURL: 'https://throne-room-club.firebaseio.com',
+    storageBucket: 'throne-room-club.appspot.com',
+    messagingSenderId: '889776405480'
   };
   firebase.initializeApp(config);
   database = firebase.database();
@@ -78,45 +78,67 @@ function setup() {
   var ref = database.ref('drawings');
   ref.on('value', gotData, errData); //trigger this anytime anything is changed in the database (err is in case of error)
   ref.once('value', buildMap, errData);  // buildMap at start
-
 }
 
 function startPath() {
   isDrawing = true; //set isdrawing to true
   currentPath = []; // reset current path to an empty object
-  tiles["1"]["drawing"].push(currentPath); // push the current path to the drawing object
+  currentTile['drawing'].push(currentPath); // push the current path to the drawing object
 }
 
 function endPath() {
   isDrawing = false; // set isdrawing to false
 }
 
-function displayDrawing() {
-  if (!drawCanvasToggle) {
-    scale(0.2, 0.2);
-  }
-  for (var i = 0; i < tiles['1']['drawing'].length; i++) { // foreach path in the drawing
-    var path = tiles["1"]["drawing"][i]; // grab the next path
+function drawTile(tile, scaleFactor) {
+  push();
+  scale(scaleFactor, scaleFactor);
+  let drawing = tile['drawing'];
+  for (let i = 0; i < drawing.length; i++) { // foreach path in the drawing
+    let path = drawing[i]; // grab the next path
     beginShape();
-    for (var j = 0; j < path.length; j++) { // for each path
+    for (let j = 0; j < path.length; j++) { // for each coordinate in the path
       vertex(path[j].x, path[j].y); // mark each vertex and draw a line between
     }
     endShape();
   }
+  pop();
 }
-function detectMouseLocation() {
-  for (const tileId in tiles) { // for each tile, check if mouse is over it
-    if (mouseX > tiles[tileId]['position']['x'] && mouseX < tiles[tileId]['position']['x'] + tiles[tileId]['width'] && mouseY > tiles[tileId]['position']['y'] && mouseY < tiles[tileId]['position']['y'] + tiles[tileId]['height']) {
-      return tiles[tileId];
+
+function displayDrawing() {
+  // no canvas: all are scaled to 0.2
+  // yes canvas: all are scaled to 0.2 except for the current tile, 1
+  if (!drawCanvasToggle) {
+    for(const tileId in tiles) {
+      drawTile(tiles[tileId], 0.2);
+    }
+  } else {
+    for (const tileId in tiles) {
+      let tile = tiles[tileId];
+      if (currentTile.tile == tileId) {
+        drawTile(tile, 1.0);
+      } else {
+        drawTile(tile, 0.2);
+      }
     }
   }
 }
+
+function detectMouseLocation() {
+  for (const tileId in tiles) { // for each tile, check if mouse is over it
+    let tile = tiles[tileId]
+    if (mouseX > tile['position']['x'] && mouseX < tile['position']['x'] + tile['width'] && mouseY > tile['position']['y'] && mouseY < tile['position']['y'] + tile['height']) {
+      return tiles[tileId];
+    }
+  }
+  console.log(`WARNING: returning undefined from detectMouseLocation - mouseX=${mouseX} mouseY=${mouseY}`);
+}
 function toggleDrawCanvas() {
-  var tileId = detectMouseLocation(); // grab mouse location (over which tile?)
+  let tile = detectMouseLocation(); // grab mouse location (over which tile?)
   if (drawCanvasToggle) { // if drawcanvas is open
-    saveDrawing(tileId); // save to specific tile
+    saveDrawing(tile); // save to specific tile
   } else { // if drawcanvas is closed
-    currentTile = tileId //update currenttile
+    currentTile = tile //update currenttile
   }
   drawCanvasToggle = !drawCanvasToggle; // toggle canvas
 }
@@ -133,13 +155,13 @@ function draw() {
   background(bg);
 
   if (drawCanvasToggle) { // if canvas is open
-    fill("white");
-    stroke("black");
+    fill('white');
+    stroke('black');
     strokeWeight(3);
     rect(20, 20, 400, 200);
     if (isDrawing) {  // if person isdrawing
       if (inDrawCanvasCheck()) { // and person isdrawing in the canvas
-        var point = { // grab the x and y of each point
+        let point = { // grab the x and y of each point
           x: mouseX,
           y: mouseY
         };
@@ -152,50 +174,44 @@ function draw() {
 }
 
 function saveDrawing(tile) {
-  var id = tile["tile"]; // grab the tile id
-  if (tiles[id]["drawing"].length > 0) { // if the drawing is not nothing
-    var ref = database.ref('drawings'); // make a new reference to the drawings database
-    var data = { // include
-      writing: 'writing',
-      drawing: drawing,
-      tile: id,
-    };
-
+  let id = tile['tile']; // grab the tile id
+  if (tiles[id]['drawing'].length > 0) { // if the drawing is not nothing
+    let ref = database.ref('drawings'); // make a new reference to the drawings database
+    function dataSent(err, status) {}
     if (tiles[id]['firebaseKey'] == null) {
-      var result = ref.push(data, dataSent); // push the data to the ref we created above
+      let result = ref.push(tile, dataSent); // push the data to the ref we created above
       tiles[id]['firebaseKey'] = result.key;
     }
-    function dataSent(err, status) {}
-    tiles[id]["drawing"] = data.drawing; // draw to the screen
   }
 }
 
 // integrate buildmap into tilemap
 function buildMap(data) {
-  var drawings = data.val(); // grab all database entries
-  var keys = drawings ? Object.keys(drawings) : []; // grab keys - if keys isn't empty
-  for (var i = 0; i < keys.length; i++) {  // for each key
-    var key = keys[i]; // grab the key
-    var tileId = drawings[key]["tile"]; // grab the tileID
-    tiles[tileId]["firebaseKey"] = key;
+  let drawings = data.val(); // grab all database entries
+  let keys = drawings ? Object.keys(drawings) : []; // grab keys - if keys isn't empty
+  for (let i = 0; i < keys.length; i++) {  // for each key
+    let key = keys[i]; // grab the key
+    let tileId = drawings[key]['tile']; // grab the tileID
+    tiles[tileId]['firebaseKey'] = key;
+    tiles[tileId]['drawing'] = drawings[key]['drawing'];
   }
 }
 
 //CALLBACK
 function gotData(data) {
   // clear the listing?
-  var elts = selectAll('.listing'); // grab all (all what?)
-  for (var i = 0; i < elts.length; i++) { // foreach
+  let elts = selectAll('.listing'); // grab all (all what?)
+  for (let i = 0; i < elts.length; i++) { // foreach
     elts[i].remove(); // remove dom elements
   }
 
-  var drawings = data.val(); // grab all drawings from firebase
-  var keys = drawings ? Object.keys(drawings) : []; // if there are keys, grab them all
-  for (var i = 0; i < keys.length; i++) { // foreach
-    var key = keys[i]; // grab the key
-    var li = createElement('li', ''); // create li element
-    li.class('listing'); // give each the "listing" class
-    var ahref = createA('#', key); // make a link element with the key in it
+  let drawings = data.val(); // grab all drawings from firebase
+  let keys = drawings ? Object.keys(drawings) : []; // if there are keys, grab them all
+  for (let i = 0; i < keys.length; i++) { // foreach
+    let key = keys[i]; // grab the key
+    let li = createElement('li', ''); // create li element
+    li.class('listing'); // give each the 'listing' class
+    // let ahref = createA('#', key); // make a link element with the key in it
     // ahref.mousePressed(showDrawing); // CREATE AN EVENT CALLED SHOW DRAWING
     var ahref = document.createElement('a');
     ahref.setAttribute('href', '#');
@@ -203,9 +219,9 @@ function gotData(data) {
     ahref.innerHTML = key;
     ahref = new p5.Element(ahref);
     ahref.parent(li);
-    // var perma = createA('?id=' + key, 'permalink'); // set up permalink
-    // perma.parent(li); // parent it to the list
-    // perma.style('padding', '4px'); // style it
+    let perma = createA('?id=' + key, 'permalink'); // set up permalink
+    perma.parent(li); // parent it to the list
+    perma.style('padding', '4px'); // style it
     li.parent('drawinglist'); // parent it to the drawing list
   }
 }
@@ -218,15 +234,19 @@ function showDrawing(key) { //show drawing
   if (key instanceof MouseEvent) { // if the key passed into showdrawing is a mouseevent
     key = key.target.innerHTML; // set key to this.html?
   }
-  var ref = database.ref('drawings/' + key); // grab the drawings and whatever key
-  ref.once('value', oneDrawing, errData); //pass the value once and run oneDrawing
 
-  function oneDrawing(data) {
-    var dbdrawing = data.val(); // grab the current drawing from the database
-    tiles["1"]["drawing"] = dbdrawing.drawing;
+  var theTileId;
+  for (const tileId in tiles) {
+    let tile = tiles[tileId];
+    if (tile.firebaseKey === key) {
+      theTileId = tileId;
+    }
   }
+  currentTile = tiles[theTileId];
+  drawCanvasToggle = true;
 }
 
+
 function clearDrawing() {
-  tiles["1"]["drawing"] = [];
+  tiles['1']['drawing'] = [];
 }
