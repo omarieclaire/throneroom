419a978a365f0d6e23f94c693dc7f681ce7c53a1
omarieclaire <marieflanagan@gmail.com>
2020-07-15 12:25:45 -0400

    test

--- js/sketch.js
+++ js/sketch.js
@@ -55,9 +55,10 @@ function setup() {
   canvas = createCanvas(900, 617);
 
   input = createInput(); // make input for text
-  writeButton = createButton('write!'); // make button for submitting text
-
-
+  writeButton = createButton('toilet thoughts'); // make button for submitting text
+// toilet thoughts - give the next person something to consider
+// what do you wish you could tell your younger self?
+// what do you want to tell the next person in this bathroom
   writeButton.mousePressed(printText);
   wordBox = createElement('h2', '');
 
@@ -90,7 +91,7 @@ function setup() {
     showDrawing(params.id);
   }
 
-  var ref = database.ref('drawings'); // get the drawings
+  var ref = database.ref('graffitiWall'); // get the graffitiWall
   ref.on('value', gotData, errData); // trigger this anytime anything is changed in the database (err is in case of error)
   ref.once('value', buildMap, errData); // buildMap at the start
 }
@@ -210,8 +211,8 @@ function displayDrawCanvas() {
   rect(drawCanvasX, drawCanvasY, drawCanvasW, drawCanvasH);
   pop();
 
-  input.position(drawCanvasX, drawCanvasY);
-  writeButton.position(input.x + input.width, drawCanvasY);
+  input.position(drawCanvasX + 10, drawCanvasY + 10);
+  writeButton.position(input.x + input.width, drawCanvasY + 10);
   wordBox.position(drawCanvasX + drawCanvasW / 2, drawCanvasY + drawCanvasY / 2);
 
 }
@@ -221,17 +222,17 @@ function displayDrawCanvas() {
 
 
 function highlightActiveTile() {
-  // push();
-  // stroke(40);
-  // stroke('red');
-  // fill('yellow');
-  // rect(currentTile.position.x, currentTile.position.y, currentTile.width, currentTile.height);
-  // pop();
+  push();
+  stroke(40);
+  stroke('blue');
+  rect(currentTile.position.x, currentTile.position.y, currentTile.width, currentTile.height);
+  pop();
 }
 
 function printText() {
-  const name = input.value();
-  wordBox.html(name);
+  const words = input.value();
+  wordBox.html(words);
+  currentTile.writing = words;
   input.value('');
 }
 
@@ -258,7 +259,7 @@ function draw() {
 function saveDrawing(tile) {
   let id = tile['tile']; // grab the tile id
   if (tiles[id]['drawing'].length > 0) { // if the drawing is not nothing
-    let ref = database.ref('drawings'); // make a new reference to the drawings database
+    let ref = database.ref('graffitiWall'); // make a new reference to the graffitiWall database
     function dataSent(err, status) {}
     if (tiles[id]['firebaseKey'] == null) {
       let result = ref.push(tile, dataSent); // push the data to the ref we created above
@@ -269,13 +270,15 @@ function saveDrawing(tile) {
 
 // integrate buildmap into tilemap
 function buildMap(data) {
-  let drawings = data.val(); // grab all database entries
-  let keys = drawings ? Object.keys(drawings) : []; // grab keys - if keys isn't empty
+  let graffitiWall = data.val(); // grab all database entries
+  let keys = graffitiWall ? Object.keys(graffitiWall) : []; // grab keys - if keys isn't empty
   for (let i = 0; i < keys.length; i++) { // for each key
     let key = keys[i]; // grab the key
-    let tileId = drawings[key]['tile']; // grab the tileID
+    let tileId = graffitiWall[key]['tile']; // grab the tileID
     tiles[tileId]['firebaseKey'] = key;
-    tiles[tileId]['drawing'] = drawings[key]['drawing'];
+    tiles[tileId]['drawing'] = graffitiWall[key]['drawing'];
+    tiles[tileId]['writing'] = graffitiWall[key]['writing'];
+
   }
 }
 
@@ -287,8 +290,8 @@ function gotData(data) {
     elts[i].remove(); // remove dom elements
   }
 
-  // let drawings = data.val(); // grab all drawings from firebase
-  // let keys = drawings ? Object.keys(drawings) : []; // if there are keys, grab them all
+  // let graffitiWall = data.val(); // grab all graffitiWall from firebase
+  // let keys = graffitiWall ? Object.keys(graffitiWall) : []; // if there are keys, grab them all
   // for (let i = 0; i < keys.length; i++) { // foreach
   // let key = keys[i]; // grab the key
   // let li = createElement('li', ''); // create li element
@@ -329,5 +332,5 @@ function showDrawing(key) { //show drawing
 
 
 function clearDrawing() {
-  tiles['1']['drawing'] = [];
+  tiles[currentTile]['drawing'] = [];
 }
